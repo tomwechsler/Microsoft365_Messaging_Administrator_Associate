584ad9a838195ebf3d1ca214b630facd3a187c86
Tom Wechsler [Microsoft Azure MVP] <56835948+tomwechsler@users.noreply.github.com>
2021-01-29 15:34:48 +0100

    Update Manage_Email_Address_Policies.ps1

--- Manage_Email_Address_Policies.ps1
+++ Manage_Email_Address_Policies.ps1
@@ -7,7 +7,7 @@ Get-EmailAddressPolicy
 New-EmailAddressPolicy -Name "Zurich Office" -IncludedRecipients AllRecipients -ConditionalStateorProvince "ZH" -EnabledEmailAddressTemplate "SMTP:%g.%s@contosotom.ch", "smtp:zh-%g.%s@contosotom.ch"
 
 #Now we use the new policy
-Update-EmailAddressPolicy–Identity "Zurich Office"
+Update-EmailAddressPolicy â€“Identity "Zurich Office"
 
 #Let's check all policies
 Get-EmailAddressPolicy
@@ -16,20 +16,20 @@ Get-EmailAddressPolicy
 Get-EmailAddressPolicy | Format-List Name,Priority,Enabled*,RecipientFilterType,IncludedRecipients,Conditional*
 
 #Check the default policy
-Get-EmailAddressPolicy –Identity Default* | Format-List Name,Priority,Enabled*,RecipientFilterType,IncludedRecipients,Conditional*
+Get-EmailAddressPolicy â€“Identity Default* | Format-List Name,Priority,Enabled*,RecipientFilterType,IncludedRecipients,Conditional*
 
 #A new policy with more specifications
-New-EmailAddressPolicy -Name "Luzern Managers" -RecipientFilter "(StateOrProvince-eq 'LU') -and (Title -like '*Director*' -or Title -like '*Manager*')" -EnabledEmailAddressTemplate "SMTP:%g.%s@contosotom.ch", "smtp:lu_managers.%g.%s@contosotom.ch"
+New-EmailAddressPolicy -Name "Luzern Managers" -RecipientFilter "(StateOrProvince -eq 'LU') -and (Title -like '*Director*' -or Title -like '*Manager*')" -EnabledEmailAddressTemplate "SMTP:%g.%s@contosotom.ch", "smtp:lu_managers.%g.%s@contosotom.ch"
 
 #Now we use the new policy
-Update-EmailAddressPolicy–Identity "Luzern Managers"
+Update-EmailAddressPolicy â€“Identity "Luzern Managers"
 
 #Update an existing policy
 Set-EmailAddressPolicy -Identity "Zurich Office" -ConditionalStateOrProvince @{Add= "ZHDWNT"}
 
 #Let's check the configuration
-Get-EmailAddressPolicy –Identity "Zurich Office" | Format-List Name,Priority,Enabled*,RecipientFilterType,IncludedRecipients,Conditional*
+Get-EmailAddressPolicy â€“Identity "Zurich Office" | Format-List Name,Priority,Enabled*,RecipientFilterType,IncludedRecipients,Conditional*
 
 #To remove a policy
-Remove-EmailAddressPolicy –Identity "Luzern Managers"
-Remove-EmailAddressPolicy –Identity "Zurich Office"
+Remove-EmailAddressPolicy â€“Identity "Luzern Managers"
+Remove-EmailAddressPolicy â€“Identity "Zurich Office"
